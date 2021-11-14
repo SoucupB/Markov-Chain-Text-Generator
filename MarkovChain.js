3a30efc6da03045d29ef2572a288aba953e1e189
unknown <bogdan.soucup@gmail.com>
2021-11-14 22:25:29 +0200

    Add rewriting difficulty

--- MarkovChain.js
+++ MarkovChain.js
@@ -1,5 +1,5 @@
 //parse the text value and put each word in an array, keep in mind that punctuation signs are also considered words by this algorithm.
-async function markovChain(text) {
+async function markovChain(text, wordsHistoryCount = 1) {
   const strToReplace = ",?!.- \n;:()[]";
   let processedString = text;
   for(let i = 0; i < strToReplace.length; i++) {
@@ -11,7 +11,7 @@ async function markovChain(text) {
       words.push(cWords[i])
     }
   }
-  let markovText = createWordsDependency(words);
+  let markovText = createWordsDependency(words, wordsHistoryCount);
   for(let i = 0; i < markovText.length; i++) {
     markovText = markovText.replaceAll(` ${strToReplace[i]} `, strToReplace[i]);
   }
@@ -31,30 +31,60 @@ function createRelativeChange(childs) {
   return countMap;
 }
 
+//combine multiple consecutive words
+function lastWords(words, index, count) {
+  let wordsString = "";
+  if(index - count + 1 < 0) {
+    return null;
+  }
+  for(let i = index - count + 1; i <= index; i++) {
+    wordsString += words[i];
+  }
+  return wordsString;
+}
+
 //it iterate through the word list and for each word it adds the next word and its counting in a dictionary.
 //This is basically a graph where each node is a word and its childs are the words that come up next in the sentence.
-function createWordsDependency(words) {
+function createWordsDependency(words, wordsHistoryCount) {
   let wordsCountMap = {};
+  for(let i = 1; i <= wordsHistoryCount; i++) {
+    wordsCountMap[i] = {};
+  }
   if(!words.length) {
     return "";
   }
-  for(let i = 0; i < words.length - 1; i++) {
-    if(!(words[i] in wordsCountMap)) {
-      wordsCountMap[words[i]] = {};
+  for(let w = 1; w <= wordsHistoryCount; w++) {
+    for(let i = w - 1; i < words.length - 1; i++) {
+      let lastWordsStr = lastWords(words, i, w);
+      if(lastWordsStr && !(lastWordsStr in wordsCountMap[w])) {
+        wordsCountMap[w][lastWordsStr] = {};
+      }
+      if(!(words[i + 1] in wordsCountMap[w][lastWordsStr])) {
+        wordsCountMap[w][lastWordsStr][words[i + 1]] = 0;
+      }
+      wordsCountMap[w][lastWordsStr][words[i + 1]]++;
     }
-    if(!(words[i + 1] in wordsCountMap[words[i]])) {
-      wordsCountMap[words[i]][words[i + 1]] = 0;
-    }
-    wordsCountMap[words[i]][words[i + 1]]++;
   }
   let response = {};
-  for(const [key, value] of Object.entries(wordsCountMap)) {
-    response[key] = createRelativeChange(value);
+  for(let i = 1; i <= wordsHistoryCount; i++) {
+    response[i] = {};
+    for(const [key, value] of Object.entries(wordsCountMap[i])) {
+      response[i][key] = createRelativeChange(value);
+    }
   }
   let responseWords = [words[0]];
   let lastWord = words[0];
   for(let i = 0; i < words.length - 1; i++) {
-    let nextWord = getNextWord(response[lastWord]);
+    let nextWord = null;
+    for(let t = wordsHistoryCount; t >= 1; t--) {
+      if(t <= i + 1) {
+        let lastWordsStr = lastWords(words, i, t);
+        nextWord = getNextWord(response[t][lastWordsStr]);
+        if(nextWord) {
+          break;
+        }
+      }
+    }
     if(!nextWord) {
       return responseWords.join(' ');
     }
@@ -98,6 +128,6 @@ function loadingRelease() {
 
 function process() {
   loadingAquire();
-  markovChain(document.getElementById('inp').value).then(finish);
+  markovChain(document.getElementById('inp').value, document.getElementById('dificulty').value).then(finish);
 }
 
